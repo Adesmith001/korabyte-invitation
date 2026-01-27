93ea56d791faa6338e64965bd5c8ab8917a436e7
Toluwani Somade <tsomade.2200314@stu.cu.edu.ng>
2026-01-27 14:23:05 +0100

    resolved build error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import WelcomeScreen from './components/WelcomeScreen'
 import EnvelopeScreen from './components/EnvelopeScreen'
 import DeclineScreen from './components/DeclineScreen'
 import { Member, Screen } from './types'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     const [currentScreen, setCurrentScreen] = useState<Screen>('landing')
